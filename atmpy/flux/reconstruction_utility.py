fix(reconstruction): Fix diffs calculation for CHI

atmpy/flux/reconstruction_utility.py
@@ -1,28 +1,52 @@
 import numpy as np
 from typing import Callable
 from atmpy.flux.utility import directional_indices, direction_mapping
+from atmpy.data.enums import PrimitiveVariableIndices as PVI
 
 
 def calculate_variable_differences(
     primitives: np.ndarray,
     ndim: int,
     direction_str: str,
-):
+) -> np.ndarray:
     """Calculate the difference of primitive variables in the given direction and store it in an array with the same
     shape as the variable array. The differences are needed to be passed to the limiter function to limit the slope.
+
+    Parameters
+    ----------
+    primitives : np.ndarray of shape (nx, [ny], [nz], num_vars)
+        The array of primitive variables.
+    ndim: int
+        The spatial dimension of the variables.
+    direction_str : str
+        The direction of the flux calculation.
+
+    Returns
+    -------
+    np.ndarray
+        The array of differences of the primitive variables with one less element.
+
+    Notes
+    -----
+    The ADVECTIVE value for scalar in the flux is not Theta anymore, it is Chi (X) so here in calculation of slopes
+    and diffs, the PVI.Y variable is Chi not Theta. Therefore the difference is calculated by the inversed values of Theta.
     """
 
     direction = direction_mapping(direction_str)
-    diffs = np.zeros_like(primitives)  # The final difference array
-    diff_values = np.diff(
-        primitives, axis=direction
-    )  # Apply np.diff in the direction which results in one less element
+    diffs = np.zeros_like(primitives)  # The final difference array]
 
     # Set the difference slice (one fewer element than the original array) in the corresponding direction
-    left_idx, _, _, _ = directional_indices(ndim, direction_str, full=False)
-    # The diffs will array now have the same shape as the primitives with the last row/column of the diff direction
-    # remaining zero
-    diffs[left_idx] = diff_values
+    left_idx, right_idx, _, _ = directional_indices(ndim, direction_str, full=False)
+
+    # Apply np.diff in the direction which results in one less element
+    # Notice the PVI.Y element is calculated differently and np.diff is not applied on it
+    diffs[..., :PVI.Y][left_idx] = np.diff(
+        primitives[..., :PVI.Y], axis=direction
+    )
+    diffs[..., PVI.Y][left_idx] = 1.0/primitives[..., PVI.Y][right_idx] - 1.0/primitives[..., PVI.Y][left_idx]
+    diffs[..., PVI.Y+1:][left_idx] = np.diff(
+        primitives[..., PVI.Y+1:], axis=direction
+    )
     return diffs
 
 
@@ -31,7 +55,7 @@ def calculate_slopes(
     direction_str: str,
     limiter: Callable[[np.ndarray, np.ndarray], np.ndarray],
     ndim: int,
-):
+) -> np.ndarray:
     """Calculate the slopes of the variables from their given difference array using the left and right values
 
     Parameters
@@ -73,7 +97,7 @@ def calculate_slopes(
 
 def calculate_amplitudes(
     slopes: np.ndarray, speed: np.ndarray, lmbda: float, left: bool
-):
+) -> np.ndarray:
     """Compute left and right state amplitudes according to BK19 paper equation 22c and 22d
 
     Parameters