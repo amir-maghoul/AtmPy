89fee5bc445854249f464fe089537284cb62c40f
Amir Maghoul <apollous.01@protonmail.com>
2025-03-27 20:50:58 +0100

    feature(discrete functions): Add new nodal_gradient function

--- atmpy/solver/discrete_functions.py
+++ atmpy/solver/discrete_functions.py
@@ -1,7 +1,8 @@
 """This module contains functions that do specific discrete jobs on the nodes or cells."""
 
 import numpy as np
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List, Tuple
+import itertools as it
 
 if TYPE_CHECKING:
     from atmpy.grid.kgrid import Grid
@@ -62,3 +63,61 @@ def momenta_divergence(grid: "Grid", variables: "Variables") -> np.ndarray:
         return Ux + Vy + Wz
     else:
         raise ValueError("Unsupported dimension {}".format(ndim))
+
+
+def nodal_gradient(p: np.ndarray, ndim: int, dxyz: List[float]):
+    """
+    Taken from reference. See Notes.
+    Calculate the discrete gradient of a given scalar field.
+
+    Parameters
+    ----------
+    p : np.ndarray
+        The scalar field on which the gradient is applied.
+    ndim : int
+        The number of dimensions of the scalar field.
+    dxyz : List[float]
+        The list of discretization fineness
+
+    Returns
+    -------
+    np.ndarray
+
+    Notes
+    -----
+    Taken from https://github.com/ray-chew/pyBELLA/blob/develop/src/dycore/physics/low_mach/second_projection.py
+    """
+    dx, dy, dz = dxyz
+
+    # Compute the slices for differencing (for example p[:-1] - p[1:])
+    indices = [idx for idx in it.product([slice(0, -1), slice(1, None)], repeat=ndim)]
+    if ndim == 2:
+        # Compute the sign factors of each neighboring cell to the center of calculation
+        # Basically in 2D we have for example in x-direction:
+        # Dpx = (-p00 - p01 + p10 + p11) * 0.5 / dx
+        signs_x: Tuple[float, ...] = (-1.0, -1.0, +1.0, +1.0)
+        signs_y: Tuple[float, ...] = (-1.0, +1.0, -1.0, +1.0)
+        signs_z: Tuple[float, ...] = (0.0, 0.0, 0.0, 0.0)
+    elif ndim == 3:
+        # Compute the sign factors of each neighboring cell to the center of calculation
+        # Basically in 3D we have for example in x-direction:
+        # Dpx = (-p000 - p001 - p010 - p011 + p100 + p101 + p110 + p111) * 0.25 / dx
+        signs_x: Tuple[float, ...] = (-1.0, -1.0, -1.0, -1.0, +1.0, +1.0, +1.0, +1.0)
+        signs_y: Tuple[float, ...] = (-1.0, -1.0, +1.0, +1.0, -1.0, -1.0, +1.0, +1.0)
+        signs_z: Tuple[float, ...] = (-1.0, +1.0, -1.0, +1.0, -1.0, +1.0, -1.0, +1.0)
+
+    Dpx, Dpy, Dpz = 0.0, 0.0, 0.0
+    cnt = 0
+
+    # Compute the unfactored gradient
+    for index in indices:
+        Dpx += signs_x[cnt] * p[index]
+        Dpy += signs_y[cnt] * p[index]
+        Dpz += signs_z[cnt] * p[index]
+        cnt += 1
+
+    Dpx *= 0.5 ** (ndim - 1) / dx
+    Dpy *= 0.5 ** (ndim - 1) / dy
+    Dpz *= 0.5 ** (ndim - 1) / dz
+
+    return Dpx, Dpy, Dpz
